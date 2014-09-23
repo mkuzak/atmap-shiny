37d69af88668a9a65312fed8d555d46271ae3594
mkuzak <mateusz.kuzak@gmail.com>
2014-09-23 21:55:15 +0200

    Render accessions on the map.

--- server.r
+++ server.r
@@ -4,10 +4,21 @@ library(leaflet)
 library(maps)
 
 # load versaille stock accessions
-accessions <- read.table("data/versaille_stock.csv", sep="\t")
+accessions <- read.table("data/versaille_stock.csv",
+                         sep="\t", header=TRUE)
 
 shinyServer(function(input, output, session) {
   accessions$id <- c(1:nrow(accessions))
 
+  # render leaflet map
   map <- createLeafletMap(session, 'map')
+  observe({
+    map$addCircle(
+      accessions$Latitude,
+      accessions$Longitude,
+      radius=rep(100000/max(5, input$map_zoom)^2, nrow(accessions)),
+      layerId=accessions$id,
+      options=list(color='#4A9', fill=TRUE, wight=1)
+    )
+  })
 })
