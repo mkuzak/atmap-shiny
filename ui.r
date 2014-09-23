e002efd6efa997339209134d4aea1ffeb98c7adf
mkuzak <mateusz.kuzak@gmail.com>
2014-09-23 22:06:54 +0200

    Zoom in on Europe.

--- ui.r
+++ ui.r
@@ -9,8 +9,8 @@ shinyUI(fluidPage(
     initialTileLayer = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
     initialTileLayerAttribution = HTML('Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
     options=list(
-      center = c(37.45, -93.85),
-      zoom = 4
+      center = c(50.92, 9.57),
+      zoom = 5
     )
   )
 ))
